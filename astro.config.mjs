a7b255f68043b9e90b8578a9715d436054b5ff83
Jean Reis <jeanreis.developer@gmail.com>
2026-05-22 18:57:34 -0300

    feat: ajustes SEO para deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,13 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://esbocoonline.com.br',
+  trailingSlash: 'ignore',
   integrations: [
     sitemap({
-      filter: (page) => !page.includes('/blob/'),
+      filter: (page) =>
+        !page.includes('/blob/') &&
+        !page.includes('/termos-de-uso/') &&
+        !page.includes('/privacidade/'),
     }),
   ],
   image: {
